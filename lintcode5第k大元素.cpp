e1a92305b74b7ce2209ee5344ac1b01a25791bf5
zzllg <296160709@qq.com>
2019-10-16 15:38:42 +0800

    Update lintcode5第k大元素.cpp

--- lintcode5第k大元素.cpp
+++ lintcode5第k大元素.cpp
@@ -10,14 +10,24 @@ public:
      */
     int kthLargestElement(int n, vector<int> &nums) {
         // write your code here
+        return partition(nums, 0, nums.size()-1, nums.size()-n);
     }
 private:
 	int partition(vector<int> &nums, int start, int end, int k){
-		if (strat >= end) return nums[start];
+		if (start >= end) return nums[k];
 		int pivot = nums[start];
-		
+		int i = start, j = end;
+		while(i < j){
+			while(nums[i] < pivot && i < j) i++;
+			while(nums[j] > pivot && i < j) j--;
+			if (i < j) swap(nums, i , j);
+		}
+		nums[i] = pivot;
+		if (k < i) return partition(nums, start, i-1, k);
+		if (k > i) return partition(nums, i+1, end, k);
+		return nums[k];
 	}
-	void swap(vector<int> &nums, i, j){
+	void swap(vector<int> &nums, int i, int j){
 		int a = nums[i];
 		nums[i] = nums[j];
 		nums[j] = a;
@@ -27,8 +37,8 @@ private:
 int main()
 {
 	Solution solve;
-	int k = 0, out = 0;
-	vector<int> nums = {1,2,3,4,5};
+	int k = 5, out = 0;
+	vector<int> nums = {100,1,2,11,4,5,10,321,4412,5315,7645,6463443,75445235,846435};
 	out = solve.kthLargestElement(k, nums);
 	cout<<out<<endl;
 }
