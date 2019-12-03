41a31e6d1dc25e58674590e24929e9e7069416c6
zzllg <zhang296160709@gmail.com>
2019-12-04 01:39:21 +0800

    lintcode7

--- lintcode7Serialize and Deserialize Binary Tree.cpp
+++ lintcode7Serialize and Deserialize Binary Tree.cpp
@@ -1,6 +1,7 @@
 #include<vector>
 #include<string>
 #include<queue>
+#include<iostream>
 using namespace std;
 
 class TreeNode {
@@ -11,7 +12,8 @@ public:
         this->val = val;
         this->left = this->right = NULL;
     }
-}
+};
+
 class Solution {
 public:
     /**
@@ -19,15 +21,15 @@ public:
      * to serialize a binary tree which denote by a root node to a string which
      * can be easily deserialized by your own "deserialize" method later.
      */
-    string serialize(TreeNode * root) {
+    string serialize(TreeNode *root) {
         // write your code here
         if(root == NULL) return "{}";
         string result;
-        vector<*node> v;
+        vector<TreeNode*> v;
         v.push_back(root);
         for(int i = 0; i < v.size(); ++i)
         {
-        	if(v[i] != NUll)
+        	if(v[i] != NULL)
 			{	
 				v.push_back(v[i]->left);
         		v.push_back(v[i]->right);
@@ -39,13 +41,12 @@ public:
         	if(v[i] == NULL) 
 			out += "#";
         	else 
-			out += to_string(v[i].val);
+			out += to_string(v[i]->val);
 			
 			if(i < v.size() - 1)
 			out += ",";
-			else
-			out += "}";
 		}
+		out += "}";
         return out;
     }
 
@@ -56,8 +57,69 @@ public:
      * designed by yourself, and deserialize it here as you serialize it in 
      * "serialize" method.
      */
+    vector<string> split(const string &s1, string s2)
+    {
+    	vector<string> out;
+    	int j;
+    	for(int i= 0; i < s1.size(); ++i)
+    	{
+    		if(s1.substr(i, 1) != s2)
+    		{
+    			j = i+1;
+    			for(j; j<s1.size(); ++j)
+    			{
+    				if(s1.substr(j,1) == s2)
+    				break;
+				}
+			}
+    		out.push_back(s1.substr(i, j-i));
+    		i = j;
+		}
+		return out;
+	}
     TreeNode * deserialize(string &data) {
         // write your code here
-        
+        if(data == "{}")
+		return NULL; 
+		vector<string> ss = split(data.substr(1,data.size()-2), ",");
+		TreeNode *root = new TreeNode(atoi(ss[0].c_str()));
+		queue<TreeNode*> Q;
+		Q.push(root);
+		bool isLeft = true;
+		for(int i = 1; i < ss.size(); ++i)
+		{
+			if(ss[i] != "#")
+			{
+				TreeNode *node = new TreeNode(atoi(ss[i].c_str()));
+				
+				if(isLeft) 
+				Q.front()->left = node;
+				else 
+				Q.front()->right = node;
+				
+				Q.push(node);
+			}
+			if(!isLeft)
+			Q.pop();
+			
+			isLeft = !isLeft;			
+		}
+		return root;
     }
 };
+
+int main()
+{
+	Solution solve;
+	TreeNode root0(0);
+	TreeNode root1(1);
+	TreeNode root2(2);
+	root0.left = &root1;
+	root0.right = &root2;
+	string out1 = solve.serialize(&root0);
+	cout<<out1<<endl;
+//	string s1 = ",";
+	vector<string> out2 = solve.split(out1, ",");
+	for(auto v : out2)
+	cout<<v<<endl;
+}
--- test.cpp
+++ test.cpp
@@ -1,9 +1,39 @@
 #include<iostream>
+#include<string>
+#include<vector> 
 using namespace std;
 
+ vector<string> split(const string &str, string delim) {
+        vector<string> results;
+        int lastIndex = 0, index;
+        while ((index = str.find(delim, lastIndex)) != string::npos) {
+            results.push_back(str.substr(lastIndex, index - lastIndex));
+            lastIndex = index + delim.length();
+        }
+        if (lastIndex != str.length()) {
+            results.push_back(str.substr(lastIndex, str.length() - lastIndex));
+        }
+        return results;
+    }
+vector<string> split1(string &s1, string s2)
+{
+	vector<string> out;
+	for(int i= 0; i < s1.size(); ++i)
+	{
+		if(s1.substr(i,1) != s2)
+		out.push_back(s1.substr(i,1));
+	}
+	return out;
+}
+
 int main(){
-	int a = 10;
-	int b = 0;
-	(a>5)?b=1:b=0;
-	cout<<b<<endl;
+	string s1 = "a,b,c,d,e";
+	string s2 = ",";
+	vector<string> out, out1;
+	out = split(s1, ",");
+	for(auto s: out)
+	cout<<s<<endl;
+	out1 = split1(s1, ",");
+	for(auto s: out1)
+	cout<<s<<endl;
 }
